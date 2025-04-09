cabc8d6ae18c6222fae8fee3b559cdebfb455fd3
unknown <M_INF_5H@ISII.PC>
2025-04-09 10:26:25 +0200

    Primo commit nel main con la struttura progetto, il .env si trova su drive

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const crypto = require("crypto");
+const postgres = require('postgres');
+
+
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+const connectionString = process.env.DATABASE_URL
+const sql = postgres(connectionString)
+
+
+// Configura CORS
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "html")));
+app.use("/css", express.static(path.join(__dirname, "css")));
+app.use("/js", express.static(path.join(__dirname, "js")));
+app.use("/img", express.static(path.join(__dirname, "img")));
+app.use(express.json());
+
